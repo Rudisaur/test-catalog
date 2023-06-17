a538683a46e00554fd7530dce553a79a25006f52
Rudolf Agaltsev <wardolfik@gmail.com>
2023-06-18 00:21:44 +0600

    refactored product view

--- resources/views/catalog.blade.php
+++ resources/views/catalog.blade.php
@@ -2,23 +2,38 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <meta name="viewport"
-          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Document</title>
+    <style>
+        .product-wrapper {
+            display: grid;
+            grid-template-columns: repeat(auto-fit, minmax(400px, 1fr));
+            gap: 20px;
+        }
 
+        .image {
+            width: 100%;
+            height: auto;
+        }
+
+        .product {
+            background-color: blanchedalmond;
+            padding: 20px;
+            margin: 0;
+        }
+    </style>
 </head>
 <body>
-<div>
+<div class="product-wrapper">
     @foreach ($products as $item)
-        <div>
+        <div class="product">
             <h2>{{ $item['name'] }}</h2>
-
-            <p>{{ $item['price'] }}</p>
-            <img src="{{ asset('storage/' . $item['link_to_image']) }}" alt="Изображение товара">
+            <h3>Цена: {{ $item['price'] }} ₸</h3>
+            <img class="image" src="{{ asset('storage/' . $item['link_to_image']) }}" alt="Изображение товара">
         </div>
     @endforeach
-
 </div>
 </body>
+
 </html>
